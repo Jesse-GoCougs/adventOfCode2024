c44c39072f04df2f4aaaa328c69f1f0553303961
Jesse-GoCougs <cruzjesus2695@gmail.com>
2024-12-11 09:42:17 -0800

    added working solution to day3

--- day3Challenge/solution.py
+++ day3Challenge/solution.py
@@ -1,24 +1,35 @@
 import re
 
-def read_input(input_file):
-    with open(input_file, 'r') as file:
-        lines = file.readlines()
-    return lines 
-        
-#part 1 of challenge 
-def sum_multiplication(memory):
+def sum_multiplication(buffer):
     pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
-    total = 0 
+    total = 0
 
-    for line in memory:
-        multiplications = re.findall(pattern, line)
-        for x, y in multiplications:
-            total += int(x) * int(y)
+    multiplications = re.findall(pattern, buffer)
+    for x, y in multiplications:
+        print(f"Processing: mul({x}, {y})")
+        total += int(x) * int(y)
     return total
+
+def process_file(input_file):
+    with open(input_file) as file:
+        line = file.read().strip()
+    matches = re.findall(r"(?:mul\((\d+),(\d+)\))|(do\(\)|don't\(\))", line)
+    print(matches)
+    enabled = True
+    ans = 0
+
+    for match in matches:
+        if match[2] == "" and enabled:
+            ans += int(match[0]) * int(match[1])
+        else:
+            if match[2] == "do()":
+                enabled = True
+            else:
+                 enabled = False
+    return ans
+
 
 
 if __name__ == "__main__":
-    testData = "}mul(620,236)where()*@}!&[mul(589,126)]&^]mul(260,42)when() when()$ ?{/^*mul(335,250)"
-    memory = read_input("day3_data.txt")
-
-    print(sum_multiplication(memory))
+    input_file = "day3_data.txt"
+    print("Total sum of enabled multiplications:", process_file(input_file))
